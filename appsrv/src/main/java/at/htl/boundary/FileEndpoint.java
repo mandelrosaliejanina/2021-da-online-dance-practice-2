f49abb4012ea12ba36db406d926fe8621b802f2a
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2021-11-18 11:19:38 +0100

    change maven.compiler.target

--- appsrv/src/main/java/at/htl/boundary/FileEndpoint.java
+++ appsrv/src/main/java/at/htl/boundary/FileEndpoint.java
@@ -13,7 +13,7 @@ import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.servlet.ServletContext;
+//import javax.servlet.ServletContext;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
@@ -36,8 +36,8 @@ public class FileEndpoint {
     @Inject
     UsageRepository usageRepository;
 
-    @Context
-    private ServletContext context;
+//    @Context
+//    private ServletContext context;
 
 
     @GET
@@ -47,46 +47,46 @@ public class FileEndpoint {
         /*JsonObject hello = Json.createObjectBuilder().add("name", "sandy").build();
         return Response.ok(hello).build();*/
     }
-
-    @POST
-    @Path("/upload")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadFile(MultipartFormDataInput input) throws IOException {
-        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
-        // Get file data to save
-        List<InputPart> inputParts = uploadForm.get("thumbnail");
-        for (InputPart inputPart : inputParts) {
-            try {
-                MultivaluedMap<String, String> header = inputPart.getHeaders();
-                String fileName = getFileName(header);
-                // convert the uploaded file to inputstream
-                InputStream inputStream = inputPart.getBody(InputStream.class, null);
-                byte[] bytes = IOUtils.toByteArray(inputStream);
-
-                /*
-                 * TODO create folder target/mediafiles/video/ or target/mediafiles/video/
-                 *
-                 * following if statement is probably not validating if the mentioned dir exists in the target folder,
-                 * which is mandatory for quarkus?
-                 *
-                 * the media files should also be store at a lfs,
-                 * because the target dir gets cleaned each time quarkus starts
-                 *
-                 * */
-
-                File customDir = new File("mediafiles/video");
-                if (!customDir.exists()) {
-                    customDir.mkdir();
-                }
-                fileName = customDir.getCanonicalPath() + File.separator + fileName;
-                writeFile(bytes, fileName);
-                return Response.status(200).entity("Uploaded file name : " + fileName+" . <br/> <a href='"+context.getContextPath()+"'>Back</a>").build();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
-    }
+//
+//    @POST
+//    @Path("/upload")
+//    @Consumes(MediaType.MULTIPART_FORM_DATA)
+//    public Response uploadFile(MultipartFormDataInput input) throws IOException {
+//        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+//        // Get file data to save
+//        List<InputPart> inputParts = uploadForm.get("thumbnail");
+//        for (InputPart inputPart : inputParts) {
+//            try {
+//                MultivaluedMap<String, String> header = inputPart.getHeaders();
+//                String fileName = getFileName(header);
+//                // convert the uploaded file to inputstream
+//                InputStream inputStream = inputPart.getBody(InputStream.class, null);
+//                byte[] bytes = IOUtils.toByteArray(inputStream);
+//
+//                /*
+//                 * TODO create folder target/mediafiles/video/ or target/mediafiles/video/
+//                 *
+//                 * following if statement is probably not validating if the mentioned dir exists in the target folder,
+//                 * which is mandatory for quarkus?
+//                 *
+//                 * the media files should also be store at a lfs,
+//                 * because the target dir gets cleaned each time quarkus starts
+//                 *
+//                 * */
+//
+//                File customDir = new File("mediafiles/video");
+//                if (!customDir.exists()) {
+//                    customDir.mkdir();
+//                }
+//                fileName = customDir.getCanonicalPath() + File.separator + fileName;
+//                writeFile(bytes, fileName);
+//                return Response.status(200).entity("Uploaded file name : " + fileName+" . <br/> <a href='"+context.getContextPath()+"'>Back</a>").build();
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return null;
+//    }
 
     private String getFileName(MultivaluedMap<String, String> header) {
         String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
