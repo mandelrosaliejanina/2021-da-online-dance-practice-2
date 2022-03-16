1ce8c8b55cf9bd7d917cbe9b01c1d944707da9f4
tangsandy <sandytang2406@gmail.com>
2022-03-16 21:10:31 +0100

    add import #danc-36

--- www/src/app/views/home/home.module.ts
+++ www/src/app/views/home/home.module.ts
@@ -14,6 +14,7 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MediaContentComponent} from './media-content/media-content.component';
 import {VideoComponent} from './media-content/video/video.component';
 import {AudioComponent} from './media-content/audio/audio.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 const routes: Routes = [
   {
@@ -43,7 +44,8 @@ const routes: Routes = [
     MatIconModule,
     FormsModule,
     FlexModule,
-    MatMenuModule
+    MatMenuModule,
+    MatFormFieldModule
   ]
 })
 export class HomeModule {
